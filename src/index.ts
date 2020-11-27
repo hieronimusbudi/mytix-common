6c08d03f04aa4454aaad687fd4aebffb39006be0
Hieronimus Budisantosa <hieronimus.budisantosa@gmail.com>
2020-11-27 11:19:23 +0700

    Updates with pub cmd

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,4 @@ export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
 export * from './events/ticket-created-events';
+export * from './events/types/order-status';
